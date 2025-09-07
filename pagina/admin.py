1c74bd31c0d99898389e86015c56e513f56bfb95
Angi-Morantes <angelicamorantes23@gmail.com>
2025-09-07 12:13:37 -0400

    configuracion del administrador

--- pagina/admin.py
+++ pagina/admin.py
@@ -4,5 +4,21 @@ from.models import User, Country, Articulo
 
 # Register your models here.
 admin.site.register(User, UserAdmin)
-admin.site.register(Country)
-admin.site.register(Articulo)
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description', 'content', 'iso_code', 'flag_photo')
+    search_fields = ('name', 'description')
+    list_editable = ( 'description', 'content', 'iso_code', 'flag_photo')
+    list_filter = ('name', 'description')
+    list_per_page = 3
+    # exclude = ('description')
+
+@admin.register(Articulo)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'description', 'image', 'places')
+    search_fields = ('title', 'description')
+    list_editable = ( 'description', 'image', 'places')
+    list_filter = ('title', 'description')
+    list_per_page = 2
+    # exclude = ('description')
